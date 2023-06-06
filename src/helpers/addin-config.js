56633423553969a9a91992b1226c88c5367fef20
Chantal Burkhard <chante11@t-online.de>
2023-06-06 18:47:19 +0200

    fixed typos and made small changes

--- src/helpers/addin-config.js
+++ src/helpers/addin-config.js
@@ -2,14 +2,14 @@ function getConfig() {
   const config = {};
 
   config.gitHubUserName = Office.context.roamingSettings.get("gitHubUserName");
-  config.defaulfGistId = Office.context.roamingSettings.get("defaultGistId");
+  config.defaultGistId = Office.context.roamingSettings.get("defaultGistId");
 
   return config;
 }
 
 function setConfig(config, callback) {
   Office.context.roamingSettings.set("gitHubUserName", config.gitHubUserName);
-  Office.context.roamingSettings.set("defaultGistId", config.defaulfGistId);
+  Office.context.roamingSettings.set("defaultGistId", config.defaultGistId);
 
   Office.context.roamingSettings.saveAsync(callback);
 }
